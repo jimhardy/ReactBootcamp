6fcc1aadad670c67d6ea39552f7063e332461deb
James Hardy <jamesthardy@gmail.com>
2019-06-02 13:01:30 +0100

    import export exercise complete

--- 09-first-react-app/src/helpers.js
+++ 09-first-react-app/src/helpers.js
@@ -1,5 +1,3 @@
-import { isEmptyStatement } from '@babel/types';
-
 // make a random selection
 function choice(items) {
     return items[Math.floor(Math.random() * items.length)];
@@ -8,14 +6,7 @@ function choice(items) {
 // remove a specific item (splice?)
 function remove(items, item) {
     const index = items.indexOf(item);
-    console.log(index);
-    items.forEach(i => {
-        let splicer = items.indexOf(i);
-        if(index === splicer){
-            console.log(`removing ${item}`);
-            items.splice(i , 1);
-        }
-    });
+            items.splice(index , 1);
 }
 
 export { choice, remove };
--- 09-first-react-app/src/index.js
+++ 09-first-react-app/src/index.js
@@ -2,6 +2,10 @@ import {choice , remove} from './helpers';
 import fruits from './food';
 
 let fruitChoice = choice(fruits);
-console.log(fruitChoice);
-
+console.log(`I would like one ${fruitChoice} please.`);
+console.log(`Here you go: ${fruitChoice}`);
 remove(fruits , fruitChoice);
+console.log('Delicious! May I have another?');
+console.log(`I'm sorry, we're all out of ${fruitChoice}. We have ${fruits.length} fruits left.`);
+console.log(fruits);
+
