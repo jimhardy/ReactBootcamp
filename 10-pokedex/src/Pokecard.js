688f8d84e345f27612e6e0ad9e6f8666ae4bada2
James Hardy <jamesthardy@gmail.com>
2019-06-05 22:23:25 +0100

    create pokecards. styling

--- 10-pokedex/src/Pokecard.js
+++ 10-pokedex/src/Pokecard.js
@@ -1,18 +1,20 @@
 // card component
 import Pokedex from './Pokedex';
 import React from 'react';
-
+import './Pokecard.css';
 
 class Pokecard extends React.Component {
-    render(){
-        return(
-                <div>
-                    <h1>{this.props.name}</h1>
-                    <p>{this.props.type}</p>
-                    <p>{this.props.xp}</p>
-                    <p>{this.props.img}</p>
-                </div>
-            );
+    render() {
+        const id = this.props.img.toString().padStart(3, '0');
+        const img = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`;
+        return (
+            <div>
+                <h2 className="Pokemon_Name">{this.props.name}</h2>
+                <img className="Pokemon_Img" src={img} alt={this.props.name} />
+                <p className="Pokemon_Type">Type: {this.props.type}</p>
+                <p className="Pokemon_EXP">EXP: {this.props.xp}</p>
+            </div>
+        );
     }
 }
 
