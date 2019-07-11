e3af68a251410cee180b87b0c34b40592b699b71
James Hardy <jamesthardy@gmail.com>
2019-07-11 22:07:43 +0100

    intro to multoforms using this.setState({ [evt.target.name]: evt.target.value });

--- 18-forms-in-react/src/Form.js
+++ 18-forms-in-react/src/Form.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
-    state = { username: '' };
+    state = { username: '', email: '', password: '' };
     handleChange = evt => {
-        this.setState({ username: evt.target.value });
+        this.setState({ [evt.target.name]: evt.target.value });
     };
     handleSubmit = evt => {
         evt.preventDefault();
         alert(`you typed ${this.state.username}`);
-        this.setState({username: ""});
-    }
+        this.setState({ username: '' });
+    };
     render() {
         return (
             <div>
                 <h1>Form Demo</h1>
                 <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="username">Username</label>
                     <input
                         type="text"
+                        name="username"
                         value={this.state.username}
                         onChange={this.handleChange}
+                        placeholder="username"
+                        id="username"
+                    />
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        placeholder="email"
+                        id="email"
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        placeholder="password"
+                        id="password"
                     />
                     <button>Submit</button>
                 </form>
