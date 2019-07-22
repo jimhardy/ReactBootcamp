b2b8e495825b958d6a2f4c49ee76d9d4564656b9
James Hardy <jamesthardy@gmail.com>
2019-07-22 21:07:16 +0100

    added dice icons, fixed handleclick function on togglelocked, but this still doesn't work as expected- seems to just lock dice in order of idx..

--- 21-yahtzee-exercise/src/App.js
+++ 21-yahtzee-exercise/src/App.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
 import Game from './Game';
 import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-    faDiceOne,
-    faDiceTwo,
-    faDiceThree,
-    faDiceFour,
-    faDiceFive,
-    faDiceSix,
-} from '@fortawesome/free-solid-svg-icons';
 
 class App extends Component {
     render() {
--- 21-yahtzee-exercise/src/Die.js
+++ 21-yahtzee-exercise/src/Die.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import './Die.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Die extends Component {
+    constructor(props) {
+        super(props);
+        this.numbers = ['', 'one', 'two', 'three', 'four', 'five', 'six'];
+    }
+    getIcon = () => `fas fa-dice-${this.numbers[this.props.val]}`;
+
     render() {
         return (
-            <button
-                className={'Die'}
-                style={{
-                    backgroundColor: this.props.locked ? 'grey' : 'black',
-                }}
-                onClick={this.props.handleClick}
-            >
-                <FontAwesomeIcon className="diceOne" />
-                {this.props.val}
-            </button>
+            <div>
+                <button
+                    className={'Die'}
+                    style={{
+                        backgroundColor: this.props.locked ? 'grey' : 'black',
+                    }}
+                    onClick={this.props.handleClick}
+                >
+                    <i className={this.getIcon()} />
+                </button>
+            </div>
         );
     }
 }
