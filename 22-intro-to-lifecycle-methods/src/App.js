8598b4eb29b0ff3db3a6937ecb36220d6b9ac938
James Hardy <jamesthardy@gmail.com>
2019-07-30 09:26:09 +0100

    setup

---
+++ 22-intro-to-lifecycle-methods/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ZenQuote from './zenQuote';
+
+function App() {
+    return (
+        <div className="App">
+            <ZenQuote />
+        </div>
+    );
+}
+
+export default App;
