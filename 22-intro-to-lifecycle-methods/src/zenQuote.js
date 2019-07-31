b2a336e7dcabd32f715a4720499f0e224bfa4074
James Hardy <jamesthardy@gmail.com>
2019-07-31 08:59:42 +0100

    loading spinner with componentDidMount

--- 22-intro-to-lifecycle-methods/src/zenQuote.js
+++ 22-intro-to-lifecycle-methods/src/zenQuote.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './zenQuote.css';
 
 class ZenQuote extends Component {
-    state = { quote: '' };
+    state = { quote: '', isLoaded: false };
     componentDidMount() {
-        axios.get('https://api.github.com/zen').then(response => {
-            console.log(response);
-            this.setState({ quote: response.data });
-        });
+        setTimeout(() => {
+            axios.get('https://api.github.com/zen').then(response => {
+                console.log(response);
+                this.setState({ quote: response.data, isLoaded: true });
+            });
+        }, 1000);
     }
     render() {
         return (
             <div>
-                <h1>Always remember that..</h1>
-                <p>{this.state.quote}</p>
+                {this.state.isLoaded ? (
+                    <div className="container">
+                        <h2 className="appear">Always remember that...</h2>
+                        <p>{this.state.quote}</p>
+                    </div>
+                ) : (
+                    <div className="loader" />
+                )}
             </div>
         );
     }
