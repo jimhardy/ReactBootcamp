4c858a8876fc3bbc6c40f762548808e4ee2b1883
James Hardy <jamesthardy@gmail.com>
2019-08-05 13:15:52 +0100

    trying to get array of jokes- currently returning unresolved promises

--- 24-dad-joke-generator-exercise/src/dadJokeGenerator.js
+++ 24-dad-joke-generator-exercise/src/dadJokeGenerator.js
@@ -2,28 +2,35 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class DadJokeGenerator extends Component {
-    static defaultProps = {
-        nJokes: 10,
-    };
     constructor(props) {
         super(props);
         this.state = {
-            jokes: [],
+            jokes: new Array(10).fill(null),
+            joke: '',
         };
+        this.jokeGetter = this.jokeGetter.bind(this);
     }
 
     async componentDidMount() {
-        const joke = await axios.get('https://icanhazdadjoke.com/', {
+        const jokeArr = await this.state.jokes.map(async joke => {
+            joke = await this.jokeGetter();
+        });
+        await this.setState(st => ({
+            jokes: jokeArr,
+        }));
+    }
+
+    async jokeGetter() {
+        return await axios.get('https://icanhazdadjoke.com/', {
             headers: { Accept: 'application/json' },
         });
-
-        console.log(joke);
     }
 
     render() {
         return (
             <div>
-                <h1>Dad Joke Generator</h1>
+                <h1>Max Joke Generator</h1>
+                <h2>{this.state.joke}</h2>
             </div>
         );
     }
