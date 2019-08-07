c57c0a918883f5a2e9f77cc970d332a4f62abb43
James Hardy <jamesthardy@gmail.com>
2019-08-07 09:09:20 +0100

    voting working. Trying to sort order based on number of votes

--- 24-dad-joke-generator-exercise/src/joke.js
+++ 24-dad-joke-generator-exercise/src/joke.js
@@ -1,39 +1,36 @@
 import React, { Component } from 'react';
 
 class DadJoke extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      joke: this.props.data
+    constructor(props) {
+        super(props);
+        this.state = {
+            joke: this.props.data,
+        };
+        this.handleVote = this.handleVote.bind(this);
+    }
+    handleUpvote = evt => {
+        this.handleVote(this.state.joke, 1);
     };
-    this.handleVote = this.handleVote.bind(this);
-  }
-  handleUpvote = evt => {
-    console.log('upvote');
-    this.handleVote(this.state, 1);
-  };
-  handleDownvote = evt => {
-    console.log('downvote');
-    this.handleVote(this.state, -1);
-  };
+    handleDownvote = evt => {
+        this.handleVote(this.state.joke, -1);
+    };
 
-  async handleVote(evt, vote) {
-    const newVotes = (await this.state.joke.votes) + vote;
-    await this.setState(joke => ({
-      joke: { ...joke, votes: newVotes }
-    }));
-    console.log(this.state);
-    this.props.handleVote(this.state.joke.joke);
-  }
-  render() {
-    return (
-      <li>
-        <button onClick={this.handleUpvote}>upvote </button>{' '}
-        <button onClick={this.handleDownvote}>downvote </button>
-        {`${this.state.joke.joke} votes: ${this.state.joke.votes}`}
-      </li>
-    );
-  }
+    async handleVote(evt, vote) {
+        const newVotes = (await this.state.joke.votes) + vote;
+        await this.setState(st => ({
+            joke: { ...st.joke, votes: newVotes },
+        }));
+        this.props.handleVote(this.state.joke);
+    }
+    render() {
+        return (
+            <li>
+                <button onClick={this.handleUpvote}>upvote </button>{' '}
+                <button onClick={this.handleDownvote}>downvote </button>
+                {`${this.state.joke.joke} votes: ${this.state.joke.votes}`}
+            </li>
+        );
+    }
 }
 
 export default DadJoke;
--- 24-dad-joke-generator-exercise/src/jokeList.js
+++ 24-dad-joke-generator-exercise/src/jokeList.js
@@ -3,70 +3,84 @@ import axios from 'axios';
 import Joke from './joke';
 
 class DadJokeGenerator extends Component {
-  static defaultProps = {
-    numJokesToGet: 10
-  };
-  constructor(props) {
-    super(props);
-    this.state = {
-      jokes: [],
-      loaded: false
+    static defaultProps = {
+        numJokesToGet: 10,
     };
-    this.jokeGetter = this.jokeGetter.bind(this);
-  }
-
-  async componentDidMount() {
-    let jokes = [];
-    let jokeSet = new Set(jokes) || 0;
-    while (jokeSet.size < this.props.numJokesToGet) {
-      let res = await this.jokeGetter();
-      const newJoke = { joke: { ...res.data, votes: 0 } };
-      // console.log(newJoke);
-      await jokes.push(newJoke);
-      jokeSet = new Set(jokes);
+    constructor(props) {
+        super(props);
+        this.state = {
+            jokes: [],
+            loaded: false,
+        };
+        this.jokeGetter = this.jokeGetter.bind(this);
     }
 
-    this.setState({ jokes: [...jokeSet], loaded: true });
-    console.log(this.state.jokes);
-  }
+    async componentDidMount() {
+        let jokes = [];
+        let jokeSet = new Set(jokes) || 0;
+        while (jokeSet.size < this.props.numJokesToGet) {
+            let res = await this.jokeGetter();
+            const newJoke = { joke: { ...res.data, votes: 0 } };
+            // console.log(newJoke);
+            await jokes.push(newJoke);
+            jokeSet = new Set(jokes);
+        }
 
-  async jokeGetter() {
-    return axios.get('https://icanhazdadjoke.com/', {
-      headers: { Accept: 'application/json' }
-    });
-  }
+        this.setState({ jokes: [...jokeSet], loaded: true });
+        console.log(this.state.jokes);
+    }
 
-  handleVote = evt => {
-    const updatedArr = this.state.jokes.map(jk => {
-      if (jk.joke.id === evt.joke.id) {
-        return { ...evt };
-      }
-      return jk;
-    });
-    this.setState({ jokes: updatedArr });
-  };
+    async jokeGetter() {
+        return axios.get('https://icanhazdadjoke.com/', {
+            headers: { Accept: 'application/json' },
+        });
+    }
 
-  render() {
-    return (
-      <div className="JokeList">
-        <h1>Generator</h1>
-        {this.state.loaded ? (
-          <div className="JoneList-Jokes">
-            {this.state.jokes.map(jk => (
-              // <div key={jk.id}>{jk.joke}</div>
-              <Joke
-                data={jk.joke}
-                key={jk.joke.id}
-                handleVote={this.handleVote}
-              />
-            ))}
-          </div>
-        ) : (
-          <p>loading...</p>
-        )}
-      </div>
-    );
-  }
+    handleVote = evt => {
+        const updatedArr = this.state.jokes
+            .map(jk => {
+                if (jk.joke.id === evt.id) {
+                    return { joke: evt };
+                }
+                return jk;
+            })
+            .sort((a, b) => {
+                return a.votes - b.votes;
+            });
+        this.setState({ jokes: updatedArr });
+    };
+
+    jokeListSort = () => {
+        const jokeList = this.state.jokes.sort((a, b) => {
+            return a.votes - b.votes;
+        });
+        console.log(jokeList);
+        this.setState({
+            jokes: jokeList,
+        });
+    };
+
+    render() {
+        return (
+            <div className="JokeList">
+                <h1>Generator</h1>
+                {this.state.loaded ? (
+                    <div className="JoneList-Jokes">
+                        {this.state.jokes.map(jk => (
+                            // <div key={jk.id}>{jk.joke}</div>
+                            <Joke
+                                data={jk.joke}
+                                key={jk.joke.id}
+                                handleVote={this.handleVote}
+                            />
+                        ))}
+                    </div>
+                ) : (
+                    <p>loading...</p>
+                )}
+            </div>
+        );
+    }
 }
 
 export default DadJokeGenerator;
