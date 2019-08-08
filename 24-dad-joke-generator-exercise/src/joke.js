e35585f1f6a799bfe8492fcba4bdd074abd85cf8
James Hardy <jamesthardy@gmail.com>
2019-08-08 20:45:22 +0100

    refactor joke component to be stateless

--- 24-dad-joke-generator-exercise/src/joke.js
+++ 24-dad-joke-generator-exercise/src/joke.js
@@ -1,39 +1,46 @@
 import React, { Component } from 'react';
 
 class DadJoke extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      joke: this.props.data
+    constructor(props) {
+        super(props);
+        this.state = {
+            joke: this.props.data,
+        };
+        this.handleVote = this.handleVote.bind(this);
+    }
+    handleUpvote = evt => {
+        this.handleVote(this.state.joke, 1);
     };
-    this.handleVote = this.handleVote.bind(this);
-  }
-  handleUpvote = evt => {
-    this.handleVote(this.state.joke, 1);
-  };
-  handleDownvote = evt => {
-    this.handleVote(this.state.joke, -1);
-  };
+    handleDownvote = evt => {
+        this.handleVote(this.state.joke, -1);
+    };
 
-  async handleVote(evt, vote) {
-    const newVotes = (await this.state.joke.votes) + vote;
-    await this.setState(st => ({
-      joke: { ...st.joke, votes: newVotes }
-    }));
-    this.props.handleVote(this.state.joke);
-  }
-  render() {
-    return (
-      <div className="Joke">
-        {`${this.state.joke.joke}  `}
-        <div className="Joke-vote-control">
-          <button onClick={this.handleUpvote}>+</button>
-          <span> {this.state.joke.votes} </span>
-        </div>
-        <button onClick={this.handleDownvote}>-</button>
-      </div>
-    );
-  }
+    async handleVote(id, vote) {
+        const newVotes = (await this.state.joke.votes) + vote;
+        await this.setState(st => ({
+            joke: { ...st.joke, votes: newVotes },
+        }));
+        this.props.handleVote(this.state.joke);
+    }
+    render() {
+        return (
+            <div className="Joke">
+                <div className="Joke-buttons">
+                    <i
+                        className="fas fa-arrow-up"
+                        onClick={this.handleUpvote}
+                    />
+                    <span> {this.state.joke.votes} </span>
+
+                    <i
+                        className="fas fa-arrow-down"
+                        onClick={this.handleDownvote}
+                    />
+                </div>
+                <div className="Joke-text">{this.state.joke.joke}</div>
+            </div>
+        );
+    }
 }
 
 export default DadJoke;
--- 24-dad-joke-generator-exercise/src/jokeList.js
+++ 24-dad-joke-generator-exercise/src/jokeList.js
@@ -2,97 +2,101 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Joke from './joke';
 import './jokeList.css';
+import './App.css';
 
 class DadJokeGenerator extends Component {
-  static defaultProps = {
-    numJokesToGet: 10
-  };
-  constructor(props) {
-    super(props);
-    this.state = {
-      jokes: [],
-      loaded: false
+    static defaultProps = {
+        numJokesToGet: 10,
     };
-    this.jokeGetter = this.jokeGetter.bind(this);
-  }
-
-  async componentDidMount() {
-    this.jokeGetter();
-  }
-
-  async jokeGetter() {
-    this.setState({ loaded: false });
-    let jokes = [];
-    let jokeSet = new Set(jokes) || 0;
-    while (jokeSet.size < this.props.numJokesToGet) {
-      let res = await axios.get('https://icanhazdadjoke.com/', {
-        headers: { Accept: 'application/json' }
-      });
-      const newJoke = { joke: { ...res.data, votes: 0 } };
-      await jokes.push(newJoke);
-      jokeSet = new Set(jokes);
+    constructor(props) {
+        super(props);
+        this.state = {
+            jokes: [],
+            loaded: false,
+        };
+        this.jokeGetter = this.jokeGetter.bind(this);
     }
-    this.setState({ jokes: [...jokeSet], loaded: true });
-  }
 
-  handleVote = evt => {
-    const updatedArr = this.state.jokes.map(jk => {
-      if (jk.joke.id === evt.id) {
-        return { joke: evt };
-      }
-      return jk;
-    });
-    const sortedArr = this.jokeListSort(updatedArr);
-    this.setState({ jokes: sortedArr });
-  };
+    async componentDidMount() {
+        this.jokeGetter();
+    }
 
-  jokeListSort = array => {
-    const ordered = array.sort((a, b) => {
-      if (a.joke.votes < b.joke.votes) {
-        return 1;
-      }
-      if (a.joke.votes > b.joke.votes) {
-        return -1;
-      } else {
-        return 0;
-      }
-    });
-    return ordered;
-  };
+    async jokeGetter() {
+        this.setState({ loaded: false });
+        let jokes = [];
+        let jokeSet = new Set(jokes) || 0;
+        while (jokeSet.size < this.props.numJokesToGet) {
+            let res = await axios.get('https://icanhazdadjoke.com/', {
+                headers: { Accept: 'application/json' },
+            });
+            const newJoke = { joke: { ...res.data, votes: 0 } };
+            await jokes.push(newJoke);
+            jokeSet = new Set(jokes);
+        }
+        this.setState({ jokes: [...jokeSet], loaded: true });
+    }
 
-  render() {
-    return (
-      <div>
-        {this.state.loaded ? (
-          <div className="JokeList">
-            <div className="JokeList-sidebar">
-              <h1>
-                <span>Dad</span> Jokes
-              </h1>
-              <img
-                className="JokeList-sidebar-emoji"
-                src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
-                alt="laughing emoji"
-              />
-              <button onClick={this.jokeGetter}>Get More Jokes</button>
+    handleVote = evt => {
+        const updatedArr = this.state.jokes.map(jk => {
+            if (jk.joke.id === evt.id) {
+                return { joke: evt };
+            }
+            return jk;
+        });
+        const sortedArr = this.jokeListSort(updatedArr);
+        this.setState({ jokes: sortedArr });
+    };
+
+    jokeListSort = array => {
+        const ordered = array.sort((a, b) => {
+            if (a.joke.votes < b.joke.votes) {
+                return 1;
+            }
+            if (a.joke.votes > b.joke.votes) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+        return ordered;
+    };
+
+    render() {
+        return (
+            <div>
+                {this.state.loaded ? (
+                    <div className="JokeList">
+                        <div className="JokeList-sidebar">
+                            <h1 className="JokeList-title">
+                                <span>Dad</span> Jokes
+                            </h1>
+                            <img
+                                src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+                                alt="laughing emoji"
+                            />
+                            <button
+                                className="JokeList-getmore"
+                                onClick={this.jokeGetter}
+                            >
+                                New Jokes
+                            </button>
+                        </div>
+                        <div className="JokeList-jokes">
+                            {this.state.jokes.map(jk => (
+                                <Joke
+                                    data={jk.joke}
+                                    key={jk.joke.id}
+                                    handleVote={this.handleVote}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                ) : (
+                    <p>loading...</p>
+                )}
             </div>
-            <div className="JokeList-jokes">
-              {this.state.jokes.map(jk => (
-                // <div key={jk.id}>{jk.joke}</div>
-                <Joke
-                  data={jk.joke}
-                  key={jk.joke.id}
-                  handleVote={this.handleVote}
-                />
-              ))}
-            </div>
-          </div>
-        ) : (
-          <p>loading...</p>
-        )}
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default DadJokeGenerator;
