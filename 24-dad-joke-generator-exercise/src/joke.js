31ece58a4b458b8b843612ea78df64fc578d2b50
James Hardy <james@Dev-MacBook-Pro.local>
2019-08-09 09:00:21 +0100

    refactored joke component to be stateless. minor layout amends

--- 24-dad-joke-generator-exercise/src/joke.js
+++ 24-dad-joke-generator-exercise/src/joke.js
@@ -1,46 +1,19 @@
 import React, { Component } from 'react';
 
 class DadJoke extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            joke: this.props.data,
-        };
-        this.handleVote = this.handleVote.bind(this);
-    }
-    handleUpvote = evt => {
-        this.handleVote(this.state.joke, 1);
-    };
-    handleDownvote = evt => {
-        this.handleVote(this.state.joke, -1);
-    };
+  render() {
+    return (
+      <div className="Joke">
+        <div className="Joke-buttons">
+          <i className="fas fa-arrow-up" onClick={this.props.upvote} />
+          <span> {this.props.votes} </span>
 
-    async handleVote(id, vote) {
-        const newVotes = (await this.state.joke.votes) + vote;
-        await this.setState(st => ({
-            joke: { ...st.joke, votes: newVotes },
-        }));
-        this.props.handleVote(this.state.joke);
-    }
-    render() {
-        return (
-            <div className="Joke">
-                <div className="Joke-buttons">
-                    <i
-                        className="fas fa-arrow-up"
-                        onClick={this.handleUpvote}
-                    />
-                    <span> {this.state.joke.votes} </span>
-
-                    <i
-                        className="fas fa-arrow-down"
-                        onClick={this.handleDownvote}
-                    />
-                </div>
-                <div className="Joke-text">{this.state.joke.joke}</div>
-            </div>
-        );
-    }
+          <i className="fas fa-arrow-down" onClick={this.props.downvote} />
+        </div>
+        <div className="Joke-text">{this.props.data.joke}</div>
+      </div>
+    );
+  }
 }
 
 export default DadJoke;
