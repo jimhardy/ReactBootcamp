8522bb9660d3beeb7fa226d27e5554abe0b592cd
James Hardy <jamesthardy@gmail.com>
2019-08-15 09:06:48 +0100

    storing jokes in local storage

--- 24-dad-joke-generator-exercise/src/jokeList.js
+++ 24-dad-joke-generator-exercise/src/jokeList.js
@@ -6,101 +6,118 @@ import './App.css';
 import FlipMove from 'react-flip-move';
 
 class DadJokeGenerator extends Component {
-  static defaultProps = {
-    numJokesToGet: 10
-  };
-  constructor(props) {
-    super(props);
-    this.state = {
-      jokes: [],
-      loaded: false
+    static defaultProps = {
+        numJokesToGet: 10,
     };
-    this.jokeGetter = this.jokeGetter.bind(this);
-    this.handleVote = this.handleVote.bind(this);
-  }
-
-  async componentDidMount() {
-    this.jokeGetter();
-  }
-
-  async jokeGetter() {
-    this.setState({ loaded: false });
-    let jokes = [];
-    let jokeSet = new Set(jokes) || 0;
-    while (jokeSet.size < this.props.numJokesToGet) {
-      let res = await axios.get('https://icanhazdadjoke.com/', {
-        headers: { Accept: 'application/json' }
-      });
-      const newJoke = { joke: { ...res.data, votes: 0 } };
-      await jokes.push(newJoke);
-      jokeSet = new Set(jokes);
+    constructor(props) {
+        super(props);
+        this.state = {
+            jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'),
+            loaded: false,
+        };
+        this.getJokes = this.getJokes.bind(this);
+        this.handleVote = this.handleVote.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
-    this.setState({ jokes: [...jokeSet], loaded: true });
-  }
 
-  async handleVote(data, delta) {
-    const updatedArr = await this.state.jokes.map(jk => {
-      if (jk.joke.id === data.id) {
-        return { joke: { ...data, votes: jk.joke.votes + delta } };
-      }
-      return jk;
-    });
-    console.log(updatedArr);
-    const sortedArr = this.jokeListSort(updatedArr);
-    this.setState({ jokes: sortedArr });
-  }
+    async componentDidMount() {
+        if (this.state.jokes.length === 0) {
+            this.getJokes();
+        } else {
+            this.setState({ loaded: true });
+        }
+    }
 
-  jokeListSort = array => {
-    const ordered = array.sort((a, b) => {
-      if (a.joke.votes < b.joke.votes) {
-        return 1;
-      }
-      if (a.joke.votes > b.joke.votes) {
-        return -1;
-      } else {
-        return 0;
-      }
-    });
-    return ordered;
-  };
+    async getJokes() {
+        this.setState({ loaded: false });
+        let jokes = [];
+        let jokeSet = new Set(jokes) || 0;
+        while (jokeSet.size < this.props.numJokesToGet) {
+            let res = await axios.get('https://icanhazdadjoke.com/', {
+                headers: { Accept: 'application/json' },
+            });
+            const newJoke = { joke: { ...res.data, votes: 0 } };
+            await jokes.push(newJoke);
+            jokeSet = new Set(jokes);
+        }
+        this.setState({ jokes: [...jokeSet], loaded: true });
+        window.localStorage.setItem('jokes', JSON.stringify(jokes));
+    }
 
-  render() {
-    return (
-      <div>
-        {this.state.loaded ? (
-          <div className="JokeList">
-            <div className="JokeList-sidebar">
-              <h1 className="JokeList-title">
-                <span>Dad</span> Jokes
-              </h1>
-              <img
-                src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
-                alt="laughing emoji"
-              />
-              <button className="JokeList-getmore" onClick={this.jokeGetter}>
-                New Jokes
-              </button>
+    async handleVote(data, delta) {
+        const updatedArr = await this.state.jokes.map(jk => {
+            if (jk.joke.id === data.id) {
+                return { joke: { ...data, votes: jk.joke.votes + delta } };
+            }
+            return jk;
+        });
+        const sortedArr = this.jokeListSort(updatedArr);
+        await this.setState({ jokes: sortedArr });
+        window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes));
+    }
+
+    jokeListSort = array => {
+        const ordered = array.sort((a, b) => {
+            if (a.joke.votes < b.joke.votes) {
+                return 1;
+            }
+            if (a.joke.votes > b.joke.votes) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+        return ordered;
+    };
+
+    handleClick() {
+        this.getJokes();
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.loaded ? (
+                    <div className="JokeList">
+                        <div className="JokeList-sidebar">
+                            <h1 className="JokeList-title">
+                                <span>Dad</span> Jokes
+                            </h1>
+                            <img
+                                src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+                                alt="laughing emoji"
+                            />
+                            <button
+                                className="JokeList-getmore"
+                                onClick={this.handleClick}
+                            >
+                                New Jokes
+                            </button>
+                        </div>
+                        <div className="JokeList-jokes">
+                            <FlipMove>
+                                {this.state.jokes.map(jk => (
+                                    <Joke
+                                        data={jk.joke}
+                                        key={jk.joke.id}
+                                        votes={jk.joke.votes}
+                                        upvote={() =>
+                                            this.handleVote(jk.joke, 1)
+                                        }
+                                        downvote={() =>
+                                            this.handleVote(jk.joke, -1)
+                                        }
+                                    />
+                                ))}
+                            </FlipMove>
+                        </div>
+                    </div>
+                ) : (
+                    <p>loading...</p>
+                )}
             </div>
-            <div className="JokeList-jokes">
-              <FlipMove>
-              {this.state.jokes.map(jk => (
-                <Joke
-                  data={jk.joke}
-                  key={jk.joke.id}
-                  votes={jk.joke.votes}
-                  upvote={() => this.handleVote(jk.joke, 1)}
-                  downvote={() => this.handleVote(jk.joke, -1)}
-                />
-              ))}
-              </FlipMove>
-            </div>
-          </div>
-        ) : (
-          <p>loading...</p>
-        )}
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default DadJokeGenerator;
