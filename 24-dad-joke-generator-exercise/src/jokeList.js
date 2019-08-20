6d4b509241b3e186ef3934c4aeb492097afdd37d
James Hardy <jamesthardy@gmail.com>
2019-08-20 08:56:46 +0100

    finalizing styling with animations

--- 24-dad-joke-generator-exercise/src/jokeList.js
+++ 24-dad-joke-generator-exercise/src/jokeList.js
@@ -120,7 +120,7 @@ class DadJokeGenerator extends Component {
                                 className="JokeList-getmore"
                                 onClick={this.handleClick}
                             >
-                                New Jokes
+                                Fetch Jokes
                             </button>
                         </div>
                         <div className="JokeList-jokes">
