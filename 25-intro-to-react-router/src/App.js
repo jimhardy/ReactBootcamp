6d39d8a3afb58a30f298cf4aafdeabafaf7d99e5
James Hardy <jamesthardy@gmail.com>
2019-08-21 09:01:10 +0100

    initial setup from source code

---
+++ 25-intro-to-react-router/src/App.js
@@ -0,0 +1,22 @@
+// ====================================================
+// IF YOU ARE LOOKING FOR THE 'fake' ROUTING EXAMPLE...
+// I MOVED THE CODE INTO FakeRouting.js
+// ====================================================
+import React, { Component } from "react";
+import { Route, Switch, NavLink } from "react-router-dom";
+import About from "./About";
+import Dog from "./Dog";
+import Contact from "./Contact";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Route path="/dog" component={Dog} />
+      </div>
+    );
+  }
+}
+
+export default App;
