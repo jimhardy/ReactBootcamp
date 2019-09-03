b9404441f54ea26ac06c2a181ed211b93670a7af
James Hardy <jamesthardy@gmail.com>
2019-09-03 09:05:20 +0100

    initial setup

---
+++ 27-react-router-patterns/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Food from './Food';
+
+function App() {
+  return (
+    <div className="App">
+      <Food name='kale' />
+    </div>
+  );
+}
+
+export default App;
