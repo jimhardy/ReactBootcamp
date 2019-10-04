2dab4466da168944cc4a77904596f09a42b24172
James Hardy <jamesthardy@gmail.com>
2019-10-04 08:49:38 +0100

    creating the react app and palette

---
+++ 29-color-picker-project/src/Palette.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+    state = {}
+    render() {
+        return (
+            <div className='palette'>
+                {/* Navbar goes here */}
+                <div className='palette-colors'>
+                    {/* Color boxes here */}
+                </div>
+                {/* Footer */}
+            </div>
+        );
+    }
+}
+
+export default Palette;
