b04c3618889dd4ba22120943667adf4277791734
James Hardy <james@Dev-MacBook-Pro.local>
2019-12-23 09:14:04 +0000

    add select and material ui

--- 29-color-picker-project/src/Navbar.js
+++ 29-color-picker-project/src/Navbar.js
@@ -1,34 +1,46 @@
 import React, { Component } from 'react';
+import Select from '@material-ui/core/Select';
 import Slider from 'rc-slider';
+import MenuItem from '@material-ui/core/MenuItem';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
-
-
 class Navbar extends Component {
-    state = {  }
-    render() { 
-        const {level , changeLevel} = this.props;
-        return ( 
-            <header className='Navbar'>
-                <div className='logo'>
-                    <a href='#'>reactColorPicker</a>
-                </div>
-                <div className='slider-container'>
-                <span>Level: {level}</span>
-                <div className='slider'> 
-                    <Slider
-                    defaultValue={level}
-                    min={100}
-                    max={900}
-                    step={100}
-                    onAfterChange={changeLevel}
-                    />
-                 </div>
-                </div>
-            </header>
-         );
-    }
+  state = { format: 'hex' };
+  handleChange = evt => {
+    this.setState({ format: evt.target.value });
+    this.props.handleChange(evt.target.value);
+  };
+  render() {
+    const { level, changeLevel } = this.props;
+    const { format } = this.state;
+    return (
+      <header className='Navbar'>
+        <div className='logo'>
+          <a href='/'>reactColorPicker</a>
+        </div>
+        <div className='slider-container'>
+          <span>Level: {level}</span>
+          <div className='slider'>
+            <Slider
+              defaultValue={level}
+              min={100}
+              max={900}
+              step={100}
+              onAfterChange={changeLevel}
+            />
+          </div>
+        </div>
+        <div className='select-container'>
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value='hex'>HEX - #ffffff</MenuItem>
+            <MenuItem value='rgb'>RGB - rgb(255, 255, 255)</MenuItem>
+            <MenuItem value='rgba'>RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
+          </Select>
+        </div>
+      </header>
+    );
+  }
 }
- 
+
 export default Navbar;
